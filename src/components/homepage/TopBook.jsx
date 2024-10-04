31592a89e6c42e99c6dde5641fde17d82686b566
Soomanib Kamruzzaman <soomanib.doict@gmail.com>
2024-10-04 20:49:24 +0600

    Added marquee for featured books

---
+++ src/components/homepage/TopBook.jsx
@@ -0,0 +1,11 @@
+const TopBook = ({url})=>{
+    return (
+        <>
+            <figure>
+                <img className="h-52 mx-4 shadow-sm" src={url}></img>
+            </figure>
+        </>
+    )
+}
+
+export default TopBook
--- src/components/homepage/TopBooks.jsx
+++ src/components/homepage/TopBooks.jsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from "react";
+import Marquee from "react-fast-marquee";
+import TopBook from "./TopBook";
+import SectionHeader from "../common/SectionHeader";
+
 const TopBooks = ()=>{
+    const [topBooks, setTopBooks] = useState([]);
+    const getTopBooks = async ()=>{
+        const topBooksJson = await fetch('json/featured.json');
+        const topBooks = await topBooksJson.json();
+        setTopBooks(topBooks);
+    }
+
+    useEffect(()=>{
+        getTopBooks();
+    }, []);
+
     return (
-        <div>
-            Carousal
-        </div>
+        <>
+            <SectionHeader headerText='Our Latest Collections'></SectionHeader>
+            <div className="border p-4">
+                <Marquee autoFill="true" pauseOnHover="true" speed={50}>
+                    {
+                        topBooks?.map((topBook)=><TopBook {...topBook}></TopBook>)
+                    }
+                </Marquee>
+            </div>
+        </>
     )
 }
 
