55d5723d1f278542c483c4c6a182d4c486367203
Soomanib Kamruzzaman <soomanib.doict@gmail.com>
2024-10-04 17:13:05 +0600

    Added contact details on footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -2,24 +2,27 @@ import { SocialIcon } from 'react-social-icons'
 
 function Footer() {
     return (
-        <footer className="footer w-full footer-center bg-base-200 text-base-content rounded p-10 border-t-2">
-            <nav>
-                <div className="grid grid-flow-col gap-4">
-                    <SocialIcon title='LinkedIn' url='https://www.linkedin.com/in/soomanib/' target='_blank'></SocialIcon>
-                    <SocialIcon title='GitHub' url='https://github.com/Raadiah' target='_blank'></SocialIcon>
-                    <SocialIcon title='LeetCode' url='https://leetcode.com/u/soomanibkz/' target='_blank'></SocialIcon>
-                </div>
-            </nav>
-            <aside>
-                <p>Copyright © 2024 - All right reserved by Reader's Cafe</p>
-                <p>
-                    <a href="https://www.flaticon.com/free-icons/bookstore" title="bookstore icons">Bookstore icons on title created by Payungkead - Flaticon</a>
-                </p>
-                <p>
-                    <a href="https://www.vecteezy.com/free-vector/404-error">404 Error Vectors by Vecteezy</a>
-                </p>
-            </aside>
-        </footer>
+        <div>
+            <footer className="footer footer-center bg-base-200 text-base-content p-4 border-t-2">
+                <aside>
+                    <p className='space-x-2'>
+                        <SocialIcon className='w-6 h-6' title='LinkedIn' url='https://www.linkedin.com/in/soomanib/' target='_blank'></SocialIcon>
+                        <SocialIcon title='GitHub' url='https://github.com/Raadiah' target='_blank'></SocialIcon>
+                        <SocialIcon title='LeetCode' url='https://leetcode.com/u/soomanibkz/' target='_blank'></SocialIcon>
+                    </p>
+                    <p>
+                        Reader's Cafe, Dhaka, Bangladesh
+                        <br />
+                        Contact: (+88)01533389101
+                    </p>
+                    <p>
+                        <a href="https://www.flaticon.com/free-icons/bookstore" title="bookstore icons">Bookstore icons on title created by Payungkead - Flaticon</a>
+                        <br />
+                        <a href="https://www.vecteezy.com/free-vector/404-error">404 Error Vectors by Vecteezy</a>
+                    </p>
+                </aside>
+            </footer>
+        </div>
     )
 }
 
