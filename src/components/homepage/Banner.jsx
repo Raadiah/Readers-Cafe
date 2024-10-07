f5145d647b01073f547c9db0598972387975aeb5
Soomanib Kamruzzaman <soomanib.doict@gmail.com>
2024-10-07 22:57:20 +0600

    Removed m-auto for banner

--- src/components/homepage/Banner.jsx
+++ src/components/homepage/Banner.jsx
@@ -8,7 +8,7 @@ function Banner() {
     hero border-b border-amber-50 text-amber-950 
     bg-teal-200 bg-gradient-to-br from-lime-50 
     max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl min-h-screen
-    z-10 shadow-lg m-auto">
+    z-10 shadow-lg">
         <div className="hero-content flex-col w-full lg:flex-row-reverse">
             <img
             src={bookImage}
