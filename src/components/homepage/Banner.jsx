5612101f91e4aef166d0e2c97e82d7c62e052bef
Soomanib Kamruzzaman <soomanib.doict@gmail.com>
2024-10-07 23:01:06 +0600

    fixed width and height of banner

--- src/components/homepage/Banner.jsx
+++ src/components/homepage/Banner.jsx
@@ -6,8 +6,7 @@ function Banner() {
   return (
     <div className="
     hero border-b border-amber-50 text-amber-950 
-    bg-teal-200 bg-gradient-to-br from-lime-50 
-    max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl min-h-screen
+    bg-teal-200 bg-gradient-to-br from-lime-50 min-h-screen
     z-10 shadow-lg">
         <div className="hero-content flex-col w-full lg:flex-row-reverse">
             <img
