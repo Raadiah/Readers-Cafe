10534e14fb5bbff169e4e211bc03316bbd4644d2
Soomanib Kamruzzaman <soomanib.doict@gmail.com>
2024-10-07 23:13:12 +0600

    add title for display photo

--- src/components/common/User.jsx
+++ src/components/common/User.jsx
@@ -2,7 +2,7 @@ const User = ({displayName, photoURL})=>{
     const displayPhotoUrl = photoURL || "https://cdn.pixabay.com/photo/2023/02/18/11/00/icon-7797704_640.png";
     return (
         <div className="flex items-center mr-2 gap-2">
-            <img className="w-8 rounded-full" src={displayPhotoUrl} alt="" />
+            <img className="w-8 rounded-full" title={displayName} src={displayPhotoUrl} alt="" />
             <span className="text-sm hidden md:flex">{displayName}</span>
         </div>
     )
