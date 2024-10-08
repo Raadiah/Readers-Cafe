5ad642416cc35922c3ab38d6f668e1d277153315
Soomanib Kamruzzaman <soomanib.doict@gmail.com>
2024-10-08 20:09:23 +0600

    Made books more responsive

--- src/components/books/Book.jsx
+++ src/components/books/Book.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom"
 import Rating from "../common/Rating"
+import { FaDollarSign } from "react-icons/fa"
 
 const Book = (book)=>{
-    const {bookId, bookName, author, image, tags, category, rating} = book
+    const {bookId, bookName, author, image, tags, category, rating, price} = book
 
     return (
-        <div className="card bg-base-100 w-80 h-96 shadow-xl">
-            <figure className="h-3/5 bg-base-300">
+        <div className="card bg-base-100 max-w-72 shrink h-full shadow-xl">
+            <figure className="h-32 bg-base-300">
                 <img
                 src={image}
                 alt={bookName} />
@@ -14,13 +15,18 @@ const Book = (book)=>{
             <div className="card-body">
                 <div className="flex items-center justify-between">
                     <div className="font-bold text-xl text-ellipsis line-clamp-1 overflow-hidden mr-2">{ bookName }</div>
-                    <div className="badge bg-pink-300 min-w-content overflow-hidden">{category}</div>
+                    <div className="badge bg-pink-300 min-w-content">{category}</div>
                 </div>
                 <h3 className="card">
                 {author}
                 </h3>
-                <div className="card-actions">  
-                    <Rating ratingValue={rating}></Rating> 
+                <div className="flex justify-between">
+                    <div className="card-actions">  
+                        <Rating ratingValue={rating}></Rating> 
+                    </div>
+                    <div className="card-actions flex items-center font-semibold">  
+                        {price} 
+                    </div>
                 </div>
                 <div className="card-actions">  
                     <p className="justify-end flex flex-wrap gap-2">
@@ -31,9 +37,9 @@ const Book = (book)=>{
                         }
                     </p> 
                 </div>
-                <div className="flex justify-center">
+                <div className="flex justify-center mt-auto mb-2">
                     <Link 
-                    className="btn btn-wide bg-white border border-teal-600 text-teal-600 hover:bg-teal-600 hover:text-white"
+                    className="btn min-w-fit w-4/5 bg-white border border-teal-600 text-teal-600 hover:bg-teal-600 hover:text-white"
                     state={{book: book}} 
                     to={`/products/${bookId}`}>Book Details</Link>
                 </div>
--- src/components/books/Books.jsx
+++ src/components/books/Books.jsx
@@ -6,9 +6,9 @@ const Books = ()=>{
     window.scrollTo(0,0);
 
     return (
-        <div id='bookList' className="lg:m-20 my-10 p-4 bg-white z-10 border ">
-            <h1 className="text-center text-2xl font-bold p-8 mb-8">Check out our latest books!</h1>
-            <div className="flex flex-wrap justify-around gap-6">
+        <div id='bookList' className="flex flex-col items-center mx-auto my-20 w-fit p-8 bg-white z-10 border rounded-2xl">
+            <h1 className="text-center text-xl font-bold mb-8">Check out our latest books!</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 w-fit gap-6">
             {
                 books?.map((book)=>(<Book key={book.id} {...book}></Book>))
             }
