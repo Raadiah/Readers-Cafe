ddc0d6aef660e0a5109a573799fe4c6cb8ffbf55
Soomanib Kamruzzaman <soomanib.doict@gmail.com>
2024-10-08 21:38:23 +0600

    added loader for login register

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,14 @@
 import { FaGoogle, FaGithub } from "react-icons/fa"
 import { Link, useLocation, useNavigate } from "react-router-dom"
 import { ROUTES } from "../routes"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import toast from "react-hot-toast";
 import { AuthContext } from "../provider/AuthProvider"
 import { GithubAuthProvider, GoogleAuthProvider } from "firebase/auth"
+import Loader from "./Loader";
 
 const Login = ()=>{
+    const [loader, setLoader] = useState(false);
     const { loginWithEmailPassword, loginWithGoogle, loginWithGitHub } = useContext(AuthContext);
     const navigate = useNavigate();
     const googleProvider = new GoogleAuthProvider();
@@ -19,37 +21,46 @@ const Login = ()=>{
         event.preventDefault();
         const email = event.target.email.value;
         const password = event.target.password.value;
+        setLoader(true);
         loginWithEmailPassword(email, password)
         .then(()=>{
             toast.success('You are successfully logged in');
+            setLoader(false);
             navigate( state ? state : ROUTES.BOOKS );
         })
         .catch((error)=>{
             toast.error('Request could not be processed')
+            setLoader(false);
             console.error(error);
         });
     }
 
     const handleGoogleLogin = ()=>{
+        setLoader(true);
         loginWithGoogle(googleProvider)
         .then(()=>{
             toast.success('You are successfully logged in with Google');
+            setLoader(false);
             navigate( state ? state : ROUTES.BOOKS );
         })
         .catch((error)=>{
             toast.error('Request could not be processed')
+            setLoader(false);
             console.error(error);
         });
     }
 
     const handleGithubLogin = ()=>{
+        setLoader(true);
         loginWithGitHub(githubProvider)
         .then(()=>{
             toast.success('You are successfully logged in with Github');
+            setLoader(false);
             navigate( state ? state : ROUTES.BOOKS );
         })
         .catch((error)=>{
             toast.error('Request could not be processed')
+            setLoader(false);
             console.error(error);
         });
     }
@@ -58,46 +69,55 @@ const Login = ()=>{
         <div className="max-w-screen-sm mx-2 my-10 md:w-3/4 md:mx-auto space-y-2 
         border p-8 md:px-32 shadow-md rounded-lg
         bg-white z-20">
-            <form onSubmit={handleLogin} className="space-y-2">
-                <div className="text-center pb-3">Login With Email</div>
-                <label className="input input-bordered flex items-center gap-2">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    fill="currentColor"
-                    className="h-4 w-4 opacity-70">
-                    <path
-                    d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-                    <path
-                    d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-                </svg>
-                <input id="email" type="text" className="grow" placeholder="Email" />
-                </label>
-                <label className="input input-bordered flex items-center gap-2">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    fill="currentColor"
-                    className="h-4 w-4 opacity-70">
-                    <path
-                    fillRule="evenodd"
-                    d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                    clipRule="evenodd" />
-                </svg>
-                <input id="password" type="password" className="grow" placeholder="******" />
-                </label>
-                <div className="flex justify-center">
-                    <button type="submit" className="btn btn-wide">Login</button>
+            {
+                loader ? 
+                <div className="w-full h-full flex items-center justify-center">
+                    <Loader></Loader>
                 </div>
-            </form>
-            <div className="divider py-6">Or Login With</div>
-            <div className="flex justify-center items-center gap-4">
-                <FaGoogle onClick={handleGoogleLogin} className="text-3xl text-red-600 cursor-pointer hover:text-red-700" title="Google"></FaGoogle>
-                <FaGithub onClick={handleGithubLogin} className="text-3xl cursor-pointer text-black hover:text-slate-700" title="GitHub"></FaGithub>
-            </div>
-            <div className="text-xs italic text-center p-2">
-                Don't have an account? <Link className="font-semibold text-cyan-700" to={ROUTES.REGISTER}>Register</Link> here.
-            </div>
+                :
+                <>
+                    <form onSubmit={handleLogin} className="space-y-2">
+                        <div className="text-center pb-3">Login With Email</div>
+                        <label className="input input-bordered flex items-center gap-2">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 16 16"
+                            fill="currentColor"
+                            className="h-4 w-4 opacity-70">
+                            <path
+                            d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                            <path
+                            d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+                        </svg>
+                        <input id="email" type="text" className="grow" placeholder="Email" />
+                        </label>
+                        <label className="input input-bordered flex items-center gap-2">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 16 16"
+                            fill="currentColor"
+                            className="h-4 w-4 opacity-70">
+                            <path
+                            fillRule="evenodd"
+                            d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                            clipRule="evenodd" />
+                        </svg>
+                        <input id="password" type="password" className="grow" placeholder="******" />
+                        </label>
+                        <div className="flex justify-center">
+                            <button type="submit" className="btn btn-wide">Login</button>
+                        </div>
+                    </form>
+                    <div className="divider py-6">Or Login With</div>
+                    <div className="flex justify-center items-center gap-4">
+                        <FaGoogle onClick={handleGoogleLogin} className="text-3xl text-red-600 cursor-pointer hover:text-red-700" title="Google"></FaGoogle>
+                        <FaGithub onClick={handleGithubLogin} className="text-3xl cursor-pointer text-black hover:text-slate-700" title="GitHub"></FaGithub>
+                    </div>
+                    <div className="text-xs italic text-center p-2">
+                        Don't have an account? <Link className="font-semibold text-cyan-700" to={ROUTES.REGISTER}>Register</Link> here.
+                    </div>
+                </>
+            }
         </div>   
     )
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,13 @@
 import { FaGoogle, FaGithub } from "react-icons/fa"
 import { Link, useNavigate } from "react-router-dom"
 import { ROUTES } from "../routes"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import toast from "react-hot-toast";
 import { AuthContext } from "../provider/AuthProvider"
+import Loader from "./Loader";
 
 const Register = ()=>{
+    const [loader, setLoader] = useState(false);
     const { createUser, updateUserProfile } = useContext(AuthContext);
     const navigate = useNavigate();
 
@@ -13,6 +15,7 @@ const Register = ()=>{
 
     const handleRegister = (event)=>{
         event.preventDefault();
+        setLoader(true);
         const email = event.target.email.value;
         const password = event.target.password.value;
 
@@ -26,11 +29,13 @@ const Register = ()=>{
             updateUserProfile(newUserProfile)
             .then(()=>{
                 toast.success("Successfully Registered")
+                setLoader(false);
                 navigate(ROUTES.BOOKS)
             })
         })
         .catch((error)=>{
             toast.error('Request could not be processed')
+            setLoader(false);
             console.error(error);
         });
     }
@@ -39,52 +44,60 @@ const Register = ()=>{
         <div className="
         max-w-screen-sm mx-2 my-10 md:w-3/4 md:mx-auto space-y-2 border p-8 md:px-32 shadow-md rounded-lg
         bg-white z-20
-        ">
-            <form className="space-y-2" onSubmit={handleRegister}>
-                <label className="input input-bordered flex items-center gap-2">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    fill="currentColor"
-                    className="h-4 w-4 opacity-70">
-                    <path
-                    d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-                </svg>
-                <input id="username" type="text" className="grow" placeholder="Full Name" />
-                </label>
-                <label className="input input-bordered flex items-center gap-2">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    fill="currentColor"
-                    className="h-4 w-4 opacity-70">
-                    <path
-                    d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-                    <path
-                    d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-                </svg>
-                <input id="email" type="text" className="grow" placeholder="Email" />
-                </label>
-                <label className="input input-bordered flex items-center gap-2">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    fill="currentColor"
-                    className="h-4 w-4 opacity-70">
-                    <path
-                    fillRule="evenodd"
-                    d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                    clipRule="evenodd" />
-                </svg>
-                <input id="password" type="password" className="grow" placeholder="******" />
-                </label>
-                <div className="flex justify-center">
-                    <button type="submit" className="btn btn-wide">Register</button>
+        ">  {
+                loader ? 
+                <div className="w-full h-full flex items-center justify-center">
+                    <Loader></Loader>
                 </div>
-            </form>
-            <div className="text-xs italic text-center p-2">
-                Already have an account? <Link className="font-semibold text-cyan-700" to={ROUTES.LOGIN}>Login</Link> here.
-            </div>
+                :
+                <>
+                <form className="space-y-2" onSubmit={handleRegister}>
+                    <label className="input input-bordered flex items-center gap-2">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 16 16"
+                        fill="currentColor"
+                        className="h-4 w-4 opacity-70">
+                        <path
+                        d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                    </svg>
+                    <input id="username" type="text" className="grow" placeholder="Full Name" />
+                    </label>
+                    <label className="input input-bordered flex items-center gap-2">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 16 16"
+                        fill="currentColor"
+                        className="h-4 w-4 opacity-70">
+                        <path
+                        d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                        <path
+                        d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+                    </svg>
+                    <input id="email" type="text" className="grow" placeholder="Email" />
+                    </label>
+                    <label className="input input-bordered flex items-center gap-2">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 16 16"
+                        fill="currentColor"
+                        className="h-4 w-4 opacity-70">
+                        <path
+                        fillRule="evenodd"
+                        d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                        clipRule="evenodd" />
+                    </svg>
+                    <input id="password" type="password" className="grow" placeholder="******" />
+                    </label>
+                    <div className="flex justify-center">
+                        <button type="submit" className="btn btn-wide">Register</button>
+                    </div>
+                </form>
+                <div className="text-xs italic text-center p-2">
+                    Already have an account? <Link className="font-semibold text-cyan-700" to={ROUTES.LOGIN}>Login</Link> here.
+                </div>
+                </>
+            }
         </div>   
     )
 }
