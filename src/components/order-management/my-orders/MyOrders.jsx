866c252253663e5b0475b2156e0f8c19405b7ff5
Soomanib Kamruzzaman <soomanib.doict@gmail.com>
2024-10-25 00:59:29 +0600

    Added total price for orders

--- src/components/order-management/my-orders/MyOrders.jsx
+++ src/components/order-management/my-orders/MyOrders.jsx
@@ -6,15 +6,18 @@ import formatDate from "../../../utils/common/dateFormatter";
 
 const MyOrders = ()=>{
     const [orders, setOrders] = useState([]);
+    const [totalPrice, setTotalPrice] = useState(0);
     const {user} = useContext(AuthContext);
     const uid = user?.uid;
-    const tableColumns = ['Book', 'Date', 'Price', 'Payment Method', 'Action'];
-    const tableColumnsClass = ['text-start min-w-72', 'text-start min-w-24', 'min-w-24', 'min-w-24'];
+    const tableColumns = ['Book', 'Date', 'Price', 'Quantity', 'Payment Method', 'Action'];
+    const tableColumnsClass = ['text-start min-w-72', 'text-start min-w-24', 'min-w-24', 'min-w-24', 'text-start min-w-24'];
     
     const fetchOrders = async()=>{
         const orderJson = await fetch(`${baseUrl}/myOrders/${uid}`)
         const orders = await orderJson.json()
+        const totalPrice = orders.map((order=>order.price*order.quantity)).reduce((currentTotal, price)=>Number(currentTotal)+Number(price))
         setOrders(orders)
+        setTotalPrice(totalPrice)
     }
 
     useEffect(()=>{
@@ -24,38 +27,40 @@ const MyOrders = ()=>{
     return (
         <div className="p-8">
             <Title title='My Orders'></Title>
+            <div className="flex justify-end text-lg mb-8">Total Price: ${totalPrice}</div>
             <table>
-            <thead>
-                <tr>
-                {
-                    tableColumns.map((tableColumn, index)=>{
-                        return(<th className={`p-2 ${tableColumnsClass[index]}`}>{tableColumn}</th>)
-                    })
-                }
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    orders.map(({image, bookName, price, orderDate, paymentMethod})=>{
-                        return(
-                            <tr>
-                                <td className="flex gap-2">
-                                    <img className="w-8 h-8" src={image}></img>
-                                    { bookName }
-                                </td>
-                                <td className="text-center">{ formatDate(orderDate) }</td>
-                                <td className="text-center">${ price }</td>
-                                <td>{ paymentMethod }</td>
-                                <td className="flex gap-2">
-                                    <button className="btn bg-green-200">
-                                        Pay Now
-                                    </button>
-                                </td>
-                            </tr>
-                        )
-                    })
-                }
-            </tbody>
+                <thead>
+                    <tr>
+                    {
+                        tableColumns.map((tableColumn, index)=>{
+                            return(<th className={`p-2 ${tableColumnsClass[index]}`}>{tableColumn}</th>)
+                        })
+                    }
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        orders.map(({image, bookName, price, quantity, orderDate, paymentMethod})=>{
+                            return(
+                                <tr>
+                                    <td className="flex gap-2 p-2">
+                                        <img className="w-8 h-8" src={image}></img>
+                                        { bookName }
+                                    </td>
+                                    <td className="text-center p-2">{ formatDate(orderDate) }</td>
+                                    <td className="text-center p-2">${ price }</td>
+                                    <td className="text-center p-2">{ quantity }</td>
+                                    <td>{ paymentMethod }</td>
+                                    <td className="flex gap-2 p-2">
+                                        <button className="btn bg-green-200">
+                                            Pay Now
+                                        </button>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
             </table>
         </div>
     )
