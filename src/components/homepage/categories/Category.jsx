bec81059d609e3e1f1e4125538e4fdfe11683f31
Soomanib Kamruzzaman <soomanib.doict@gmail.com>
2024-10-26 16:52:56 +0600

    Category card made according to the requirement

--- src/components/homepage/categories/Category.jsx
+++ src/components/homepage/categories/Category.jsx
@@ -2,17 +2,28 @@ import { Link } from "react-router-dom"
 import { ROUTES } from "../../../routes";
 
 const Category = ({category})=>{
-    return(
-        <Link
-        to={`${ROUTES.BOOKS}/${category}`}
-        className="carousel-item w-52 h-44 z-20 flex justify-center items-center">
-            <div className="w-52 h-44 shadow-inner
-            flex justify-center items-center
-            font-bold border border-black cursor-pointer rounded-lg
-            hover:w-48 hover:h-40 hover:bg-green-50">
-                {category}
+    return(      
+        <div 
+        className="flex justify-center items-center
+        carousel-item w-52 h-44 z-20">
+            <div 
+            className="group card-body
+            w-52 h-44 shadow-inner cursor-pointer
+            flex justify-center items-center gap-4
+            font-bold border border-black rounded-lg
+            hover:bg-green-50 hover:shadow-xl">
+                <h2 className="card-title">{category}</h2>
+                <div className="card-actions justify-end">
+                <Link
+                to={`${ROUTES.BOOKS}/${category}`}
+                className="btn bg-white border border-teal-600 text-teal-600 
+                group-hover:bg-teal-600 group-hover:text-white hover:shadow-xl"
+                >
+                    View Products
+                </Link>
+                </div>
             </div>
-        </Link>
+        </div>
     )
 }
 
