95a391f40cb100009ea9139aa89e6614bbd80920
Soomanib Kamruzzaman <soomanib.doict@gmail.com>
2024-10-27 18:06:25 +0600

    installed react helmet

--- src/main.jsx
+++ src/main.jsx
@@ -9,12 +9,15 @@ import {
 } from "react-router-dom";
 import AuthProvider from './provider/AuthProvider.jsx';
 import { Toaster } from 'react-hot-toast';
+import { HelmetProvider } from 'react-helmet-async';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-      <Toaster></Toaster>
-    </AuthProvider>
+    <HelmetProvider>
+      <AuthProvider>
+        <RouterProvider router={router} />
+        <Toaster></Toaster>
+      </AuthProvider>
+    </HelmetProvider>
   </StrictMode>,
 )
