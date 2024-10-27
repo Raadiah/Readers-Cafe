3a9eccff8b3ac36ea3045562bcf67b66e9e7fd5c
Soomanib Kamruzzaman <soomanib.doict@gmail.com>
2024-10-27 10:39:39 +0600

    added image bb site on sites

--- src/routes/sites.js
+++ src/routes/sites.js
@@ -1,6 +1,7 @@
-const SITES = {
+export const SITES = {
     DEV: 'http://localhost:5000',
-    PROD: 'https://readers-cafe-backend.vercel.app'
+    PROD: 'https://readers-cafe-backend.vercel.app',
+    IMAGE_BB_SITE: 'https://api.imgbb.com/1/upload'
 }
 
 const baseUrl = SITES.PROD
